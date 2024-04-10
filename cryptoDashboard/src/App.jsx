10bd36fb98c601abf4c5218ad90fc10125b14e47
Umutcany <gleen_pro@hotmail.com>
2024-04-10 18:57:05 +0300

    Step3

--- cryptoDashboard/src/App.jsx
+++ cryptoDashboard/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Common/Header/Header";
 
 function App() {
-  return <>HEY!</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
